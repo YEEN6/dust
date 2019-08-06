60cc786a4eca8d4cd80a5bcf7f0cacf416e39b8f
YEEN6 <yeen666@kookmin.ac.kr>
2019-08-07 03:23:31 +0900

    initial settings

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask import render_template
+from flask import url_for
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+with app.test_request_context():
+    print(url_for('index'))
